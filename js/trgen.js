0810efbf5c83dad26248e7efe878b72c2c5bf3f2
Pinzoney <136270873+Pinzoney@users.noreply.github.com>
2025-11-14 17:50:44 -0600

    Minor Changes

--- js/trgen.js
+++ js/trgen.js
@@ -1,5 +1,6 @@
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
 const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+var dispBtns = document.querySelectorAll("button[name=dispBtn]")
 var trReason = document.getElementById("trReaSel")
 var cxState = document.getElementById("cxState")
 var tsSteps = document.getElementById("tsSteps")
@@ -13,24 +14,34 @@ var btnGen = document.getElementById("genBut")
 
 var prefillData = {
     'hdONT': {
+        issue: "",
         cxStatement: "Hard Down ONT, lights are on but internet is not working",
         troubleshootingSteps: " -Power cycled ONT\n -No damage to patch fiber cable\n -No damage to drop bury",
     },
     'cutFiber': {
-        cxStatement: "",
-        troubleshootingSteps: "",
+        issue: "",
+        cxStatement: "Cx reports that their fiber drop was cut and needs it replaced",
+        troubleshootingSteps: " -No damage to patch fiber cable\n -Cx visually confirmed their fiber cable was damaged\n -Creating task to have contractor fix their drop",
     },
     'dmgPatch': {
-        cxStatement: "",
-        troubleshootingSteps: "",
+        issue: "",
+        cxStatement: "Cx reports that they have no internet and modem is on with lights",
+        troubleshootingSteps: " -Power cycled ONT, no fix\n -Cx confirmed their patch fiber cable is damaged/cut\n -Need FST visit to change their patch fiber cable",
     },
     'lowLight': {
-        cxStatement: "",
-        troubleshootingSteps: "",
+        issue: "",
+        cxStatement: "Cx reports intermittent drops, issues with buffering, issues with latency or long loading times for apps/websites",
+        troubleshootingSteps: " -Power cycled ONT and router, no fix\n -ONT has low light levels which cause the modem to ocassionally drop\n -TR to check light levels in the area",
     },
     'swapRouters': {
-        cxStatement: "",
-        troubleshootingSteps: "",
+        issue: "",
+        cxStatement: "Cx is running into issues with their Wi-fi randomly crashing/not working, {Cx has PB70s/Cx has EVOs}",
+        troubleshootingSteps: " -Power cycled ONT and router, no fix\n -No damage to patch fiber cable\n -Plume shows crashing due to {reason}\n -requesting FST to review their hardware and possible swap their routers out",
+    },
+    'repONT': {
+        issue: "",
+        cxStatement: "Cx requests to reposition their modem to a different area",
+        troubleshootingSteps: " -Cx requests to move their modem since they think they could get better signal/it's installed in a enclosed space and could be having heating issues\n -Requesting FST to assist in modem their modem to a different area is possible",
     },
 };
 
@@ -45,6 +56,10 @@ trReason.addEventListener('change', function() {
     tsSteps.value = selTrData.troubleshootingSteps;
 })
 
+dispBtns.addEventListener('click', function() {
+    nDisp.value = this.value;
+});
+
 btnPaste.addEventListener('click', function () {
     const ontStatsText = document.getElementById("ontStats");
         var oriHTML = btnPaste.innerHTML
@@ -54,8 +69,16 @@ btnPaste.addEventListener('click', function () {
 
 btnGen.addEventListener('click', function () {
     var str;
+        var selValue = trReason.value
+    var selTrData = prefillData[selValue];
+    if(!selTrData) {
+        console.log('No data found for option: ' + selValue);
+        return
+    }
+    cxState.value = selTrData.cxStatement;
+    tsSteps.value = selTrData.troubleshootingSteps;
     str =
-        `Issue: ${trReason.value}
+        `Issue: ${selTrData.issue}
 
 Cx Statement: ${cxState.value}
             
