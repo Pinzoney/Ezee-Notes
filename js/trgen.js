ecf21f2af5fa4bed814460ebcd448c10b37f1c17
Pinzoney <136270873+Pinzoney@users.noreply.github.com>
2025-11-14 22:56:15 -0600

    TR page done, logic is working and scalable to different issues or fields

--- js/trgen.js
+++ js/trgen.js
@@ -14,32 +14,26 @@ var btnGen = document.getElementById("genBut")
 
 var prefillData = {
     'hdONT': {
-        issue: "",
         cxStatement: "Hard Down ONT, lights are on but internet is not working",
         troubleshootingSteps: " -Power cycled ONT\n -No damage to patch fiber cable\n -No damage to drop bury",
     },
     'cutFiber': {
-        issue: "",
         cxStatement: "Cx reports that their fiber drop was cut and needs it replaced",
         troubleshootingSteps: " -No damage to patch fiber cable\n -Cx visually confirmed their fiber cable was damaged\n -Creating task to have contractor fix their drop",
     },
     'dmgPatch': {
-        issue: "",
         cxStatement: "Cx reports that they have no internet and modem is on with lights",
         troubleshootingSteps: " -Power cycled ONT, no fix\n -Cx confirmed their patch fiber cable is damaged/cut\n -Need FST visit to change their patch fiber cable",
     },
     'lowLight': {
-        issue: "",
         cxStatement: "Cx reports intermittent drops, issues with buffering, issues with latency or long loading times for apps/websites",
         troubleshootingSteps: " -Power cycled ONT and router, no fix\n -ONT has low light levels which cause the modem to ocassionally drop\n -TR to check light levels in the area",
     },
     'swapRouters': {
-        issue: "",
         cxStatement: "Cx is running into issues with their Wi-fi randomly crashing/not working, {Cx has PB70s/Cx has EVOs}",
         troubleshootingSteps: " -Power cycled ONT and router, no fix\n -No damage to patch fiber cable\n -Plume shows crashing due to {reason}\n -requesting FST to review their hardware and possible swap their routers out",
     },
     'repONT': {
-        issue: "",
         cxStatement: "Cx requests to reposition their modem to a different area",
         troubleshootingSteps: " -Cx requests to move their modem since they think they could get better signal/it's installed in a enclosed space and could be having heating issues\n -Requesting FST to assist in modem their modem to a different area is possible",
     },
@@ -56,8 +50,10 @@ trReason.addEventListener('change', function() {
     tsSteps.value = selTrData.troubleshootingSteps;
 })
 
-dispBtns.addEventListener('click', function() {
-    nDisp.value = this.value;
+dispBtns.forEach(function(button) {
+    button.addEventListener('click', function() {
+        nDisp.value = this.value;
+    })
 });
 
 btnPaste.addEventListener('click', function () {
@@ -78,9 +74,7 @@ btnGen.addEventListener('click', function () {
     cxState.value = selTrData.cxStatement;
     tsSteps.value = selTrData.troubleshootingSteps;
     str =
-        `Issue: ${selTrData.issue}
-
-Cx Statement: ${cxState.value}
+        `Cx Statement: ${cxState.value}
             
 Troubleshooting Steps: \n${tsSteps.value}
             
